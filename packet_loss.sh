038f196cf4b4cff5ab991cdf24014e6a6e585da7
jaclu <jacob.lundqvist@gmail.com>
2021-11-14 23:48:48 +0100

    Added 2nd param host. Ensure host is responding when starting. Shortened output lines, so they can run in a 28 col terminal. Uses timeout of ping_count * 1.5 rounded for ping.

--- packet_loss.sh
+++ packet_loss.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
+__version="1.5.1 2021-11-14"
 #
-# Copyright (c) 2020,2021: Jacob.Lundqvist@gmail.com 2021-10-02
+# Copyright (c) 2020,2021: Jacob.Lundqvist@gmail.com
 # License: MIT
 #
 # Part of https://github.com/jaclu/helpfull_scripts
 #
-#  Version: 1.4.1 2021-10-02
-#      Switched shell from bash to /bin/sh
-#      now prints total ping count and losses upon Ctrl-C termination
-#      Fixed emacs ruined indents again, need to check my emacs config...
-#  1.4.0  2020-08-11
-#      Added support for multi route testing
+#  Version: $__version
+#       Ensure host is responding when starting.
+#       Shortened output lines, so they can run in a 28 col terminal.
+#       Corrected printout after Ctrl-C to point out what is included.
+#       Changed min allowed count into 1.
+#   1.5.1 2021-11-11
+#       Uses timeout of ping_count * 1.5 rounded for ping
+#       Does requested amount of pings again.
+#   1.5.0 2021-11-10
+#       Added 2nd param host.
+#       increased min ping_count since low numbers tende
+#       to give false negatives.
+#       Does one more ping than ping count, since first ping is
+#       sent at time 0, to line up timestamps with ping count
+#       I.E 5 will display status every 5s and so on.
+#       reduced output to fit a smaller width
+#   1.4.1 2021-10-02
+#       Switched shell from bash to /bin/sh
+#       now prints total ping count and losses upon Ctrl-C termination
+#       Fixed emacs ruined indents again, need to check my emacs config...
+#   1.4.0  2020-08-11
+#       Added support for multi route testing
 #
 #  Displays packet loss over time, see below for more usage hints
 #
@@ -20,10 +37,10 @@
 #  ie how often you will get updated
 #  can be overridden by param1
 #
-ping_count=300
+ping_count=6
 
 #
-# what to ping
+# what to ping (can be overriden by param2)
 #
 host=8.8.4.4
 
@@ -83,49 +100,69 @@ host=8.8.4.4
 #
 #  Override ping_count with param 1
 #
-if [ $# = 1 ] ; then
-    ping_count=$1
-    # case "${1#[+-]}" in
-    case "$ping_count" in
-        (*[!0123456789]*)
-            echo "ERROR param 1 not int value!"
-            exit 1
-            ;;
-    esac
-    if [ "$ping_count" -lt 2 ]; then
-        echo "***  $ping_count is not a meaningfull value, changed to 2  ***"
-        ping_count=2
-    fi
-elif [ $# -gt 1 ] ; then
-    echo "*** Only supports one param - to overide default amount" \
-        "of pings per test."
+if [ $# -gt 2 ] ; then
+    echo "*** Only supports two params - pingcount and host."
     exit 1
 fi
+
+if [ -n  "$1" ] ; then
+    ping_count="$1"
+    case "$ping_count" in
+
+        (*[!0123456789]*)
+            echo "ERROR param 1 not a valid integer value!"
+            exit 1
+            ;;
+
+    esac
+    if [ "$ping_count" -lt 1 ]; then
+        echo "***  $ping_count is not a meaningfull value, changed to 1  ***"
+        ping_count=1
+    fi
+fi
+
+if [ -n "$2" ]; then
+    host="$2"
+fi
+
+
+#
+# Ensure host is up when we start
+#
+ping -c 1 -t 1 "$host" > /dev/null
+if [ "$?" -ne 0 ]; then
+    echo "ERROR: host: $host is not responding!"
+    exit 1
+fi
+
 
 
 #  Kill this script on Ctrl-C, dont let ping swallow it
 trap '
     echo
-    echo "performed $(( iterations * ping_count )) pings, total packet loss: $ack_loss"
+    echo "Stats up to last printout:"
+    echo "  performed $(( iterations * ping_count )) pings, " \
+         "total packet loss: $ack_loss"
     trap - INT # restore default INT handler
     kill -s INT "$$"
 ' INT
 
-echo "This will report packet loss to $host every $ping_count seconds" \
-     "with a timestamp"
-if [ $# -ne 1 ] ; then
-    echo "optional param 1 overrides default ($ping_count) interval"
-fi
+echo "$(basename $0) version: $__version"
+echo "This will ping once per second and report packet loss"
+echo "to $host every $ping_count packets with a timestamp"
 
 
-#
-#  This will run $ping_count pings to $host and then report packet loss
-#
+timeout="$(echo "$ping_count * 1.5 + 0.49" | bc | cut -d'.' -f 1)"
+
 iterations=0
 while true; do
-    iterations=$(( iterations + 1 ))
-    output="$( ping -c $ping_count -t $ping_count $host 2> /dev/null |
+    #
+    #  This will run $ping_count pings to $host and then report packet loss
+    #  and then repeat this until Ctrl-C is hit
+    #
+    output="$( ping -c $ping_count -t $timeout  $host 2> /dev/null |
                grep loss)"
+    iterations=$(( iterations + 1 ))
     this_time_packet_loss=$(echo "$output" | awk '{print $1-$4}')
     this_time_percent_loss=$(echo "$output" | awk '{print $7}')
     ack_loss=$((ack_loss + this_time_packet_loss))
@@ -134,7 +171,7 @@ while true; do
             -v ping_count=$ping_count \
             'BEGIN { print 100 * (ack_loss/(count * ping_count)) }' )
 
-    printf "%6s avg:%3.0f%%   %ss - " "$this_time_percent_loss" "$avg_loss" \
+    printf "%6s avg:%3.0f%% %s " "$this_time_percent_loss" "$avg_loss" \
            "$ping_count"
-    date
+    date | awk '{ print $5 }'
 done
