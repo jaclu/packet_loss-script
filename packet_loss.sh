33e92ca3315e6af324240401bc97cf8a1e49fbf6
jaclu <jacob.lundqvist@gmail.com>
2021-11-15 02:17:19 +0100

    Added check if ping is the busybox version, without timeout param and deals with it. Removed timeout calculations, didn't make sense.

--- packet_loss.sh
+++ packet_loss.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-__version="1.5.1 2021-11-14"
+__version="1.5.3 2021-11-14"
 #
 # Copyright (c) 2020,2021: Jacob.Lundqvist@gmail.com
 # License: MIT
@@ -7,6 +7,10 @@ __version="1.5.1 2021-11-14"
 # Part of https://github.com/jaclu/helpfull_scripts
 #
 #  Version: $__version
+#       Added check if ping is the busybox version,
+#       without timeout param and deals with it.
+#       Removed timeout calculations, didn't make sense.
+#   1.5.2 2021-11-14
 #       Ensure host is responding when starting.
 #       Shortened output lines, so they can run in a 28 col terminal.
 #       Corrected printout after Ctrl-C to point out what is included.
@@ -37,56 +41,12 @@ __version="1.5.1 2021-11-14"
 #  ie how often you will get updated
 #  can be overridden by param1
 #
-ping_count=6
+ping_count=60
 
 #
 # what to ping (can be overriden by param2)
 #
 host=8.8.4.4
-
-
-#
-#  In the default case where you just want to see average packet loss
-#  over time, nothing further needs to be done but starting this script.
-#
-#  I tend to use 8.8.8.8 (google DNS as my general ping check
-#  Here I use 8.8.4.4 (Google secondary DNS).
-#  This way I can route 8.8.4.4 through a troubled link whilst
-#  Using a prefered link as my default route. In order to get better
-#  net service for most of my network needs and still be able to track
-#  this specific node through the troubled connection.
-#
-#  If you have two access points and want to keep track of "the other",
-#  I woulld< suggest the following:
-#
-#  In MacOs:
-#
-#  1. In most cases you need to diable vpn, since they tend to
-#     disregard specific routes, and insist on routing everything
-#     except the local network through the vpn.
-#  2. System Preferences - Network - cogwheel at the bottom of the
-#     left frame - Set Service Order...
-#     This list essentially decides wich connection will be given the
-#     role of default route, if multiple connections are available.
-#     Make sure the service you prefer as the default route is at
-#     the top of the list, or at the very least above the troubled link
-#  3. Hit OK to close the popup
-#  4. Hit Apply to save the settings to the network routing layer.
-#  5. In a terminal route 8.8.4.4 through the troubled link.
-#     For me it tends to be my Ziggo router, in the example bleow
-#     having the IP# 192.168.178.1
-#
-#    sudo route add -gateway 192.168.178.1 -host 8.8.4.4
-#
-#  6. From now all traffic to 8.8.4.4 follows this route,
-#     and can be used to monitor the packet loss of that link
-#     Without forcing you to use it as a deault route
-#  7. You can still moniotor packetloss on yor primary
-#     route by doing something like:  ping 8.8.8.8
-#  8. If you changed the order in "Set Service order" above,
-#     remmebr to rearange the order back to your normally prefered
-#     state once you are done Testing using a multi path setup!
-
 
 
 #==========================================
@@ -96,12 +56,14 @@ host=8.8.4.4
 #==========================================
 
 
+echo "$(basename $0) version: $__version"
+
 
 #
 #  Override ping_count with param 1
 #
 if [ $# -gt 2 ] ; then
-    echo "*** Only supports two params - pingcount and host."
+    echo "ERROR: Only params supported - pingcount and host."
     exit 1
 fi
 
@@ -116,28 +78,69 @@ if [ -n  "$1" ] ; then
 
     esac
     if [ "$ping_count" -lt 1 ]; then
-        echo "***  $ping_count is not a meaningfull value, changed to 1  ***"
+        echo "WARNING: $ping_count is not a meaningfull value, changed to 1  ***"
         ping_count=1
     fi
 fi
 
+
+#
+#   Override default host with param 2
+#
 if [ -n "$2" ]; then
     host="$2"
 fi
 
 
 #
-# Ensure host is up when we start
+#  Check if this ping supports timeout.
 #
-ping -c 1 -t 1 "$host" > /dev/null
-if [ "$?" -ne 0 ]; then
-    echo "ERROR: host: $host is not responding!"
-    exit 1
+if [ -z "$(ls -l $(which ping)|grep busybox)" ]; then
+    time_out_supported=1
+    ping_cmd="ping -t 1"
+else
+    #
+    # busybox ping does not support ping timeout...
+    #
+    time_out_supported=0
+    ping_cmd="ping"
+    echo
+    echo "WARNING: This ping does not support a timeout param!"
+    echo
+    echo "  This means that each time when at first the host is not responding,"
+    echo "  there will be an initial 10 second timeout."
+    echo "  Concecutive failed pings will only take one second."
+    echo "  If the host reglarly fails to respond the accumulated first 10 seconds"
+    echo "  will add-up and skew the printouts"
+    echo
 fi
 
 
+#
+#  Check if host is initially responding.
+#
+$ping_cmd -c 1 "$host" > /dev/null
+if [ "$?" -ne 0 ]; then
+    echo
+    echo "WARNING: host: $host is not responding!"
+    echo
+fi
 
+
+echo "This will ping once per second and report packet loss with"
+printf "$host every $ping_count packets"
+
+if [ "$time_out_supported" -eq 1 ]; then
+    ping_cmd="ping -t $ping_count"
+    echo ", timing out after $ping_count seconds."
+else
+    echo "."
+fi
+echo
+
+#
 #  Kill this script on Ctrl-C, dont let ping swallow it
+#
 trap '
     echo
     echo "Stats up to last printout:"
@@ -147,20 +150,14 @@ trap '
     kill -s INT "$$"
 ' INT
 
-echo "$(basename $0) version: $__version"
-echo "This will ping once per second and report packet loss"
-echo "to $host every $ping_count packets with a timestamp"
-
-
-timeout="$(echo "$ping_count * 1.5 + 0.49" | bc | cut -d'.' -f 1)"
 
 iterations=0
 while true; do
     #
-    #  This will run $ping_count pings to $host and then report packet loss
-    #  and then repeat this until Ctrl-C is hit
+    #  This will run $ping_count pings to $host and then report packet loss.
+    #  This will be repated until Ctrl-C
     #
-    output="$( ping -c $ping_count -t $timeout  $host 2> /dev/null |
+    output="$( $ping_cmd -c $ping_count $host 2> /dev/null |
                grep loss)"
     iterations=$(( iterations + 1 ))
     this_time_packet_loss=$(echo "$output" | awk '{print $1-$4}')
@@ -173,5 +170,5 @@ while true; do
 
     printf "%6s avg:%3.0f%% %s " "$this_time_percent_loss" "$avg_loss" \
            "$ping_count"
-    date | awk '{ print $5 }'
+    date +%H:%M:%S
 done
